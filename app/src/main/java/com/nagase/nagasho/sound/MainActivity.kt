79be30c75fdd2a225b2d859cc108ecec6cdb8f77
nagasho <abc@example.com>
2021-04-27 12:32:33 +0900

    サウンドアプリ完成3

--- app/src/main/java/com/nagase/nagasho/sound/MainActivity.kt
+++ app/src/main/java/com/nagase/nagasho/sound/MainActivity.kt
@@ -12,6 +12,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val drumSound = MediaPlayer.create(this, R.raw.drum_sound)
+        val guitarSound = MediaPlayer.create(this, R.raw.guitar_sound)
+        val pianoSound = MediaPlayer.create(this, R.raw.piano_sound)
 
         drumImage.setOnTouchListener { view, event ->
             if (event.action == MotionEvent.ACTION_DOWN){
@@ -23,6 +25,32 @@ class MainActivity : AppCompatActivity() {
             }
             else if (event.action == MotionEvent.ACTION_UP){
                 drumImage.setImageResource(R.drawable.drum_image)
+            }
+            true
+        }
+        guitarImage.setOnTouchListener { view, event ->
+            if (event.action == MotionEvent.ACTION_DOWN){
+                guitarImage.setImageResource(R.drawable.guitar_playing_image)
+                //再生音を初めに戻す
+                guitarSound.seekTo(0)
+
+                guitarSound.start()
+            }
+            else if (event.action == MotionEvent.ACTION_UP){
+                guitarImage.setImageResource(R.drawable.guitar_image)
+            }
+            true
+        }
+        pianoImage.setOnTouchListener { view, event ->
+            if (event.action == MotionEvent.ACTION_DOWN){
+                pianoImage.setImageResource(R.drawable.piano_playing_image)
+                //再生音を初めに戻す
+                pianoSound.seekTo(0)
+
+                pianoSound.start()
+            }
+            else if (event.action == MotionEvent.ACTION_UP){
+                pianoImage.setImageResource(R.drawable.piano_image)
             }
             true
         }
