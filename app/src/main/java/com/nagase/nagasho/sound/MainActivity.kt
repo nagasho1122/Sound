4d3d9aada8ac3028239ccaed15a4e867786708fa
nagasho <abc@example.com>
2021-04-27 11:30:23 +0900

    サウンドアプリ完成2

--- app/src/main/java/com/nagase/nagasho/sound/MainActivity.kt
+++ app/src/main/java/com/nagase/nagasho/sound/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nagase.nagasho.sound
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +13,20 @@ class MainActivity : AppCompatActivity() {
 
         val drumSound = MediaPlayer.create(this, R.raw.drum_sound)
 
-        drumImage.setOnClickListener {
-            drumSound.seekTo(0)
-            drumSound.start()
+        drumImage.setOnTouchListener { view, event ->
+            if (event.action == MotionEvent.ACTION_DOWN){
+                drumImage.setImageResource(R.drawable.drum_playing_image)
+                //再生音を初めに戻す
+                drumSound.seekTo(0)
+
+                drumSound.start()
+            }
+            else if (event.action == MotionEvent.ACTION_UP){
+                drumImage.setImageResource(R.drawable.drum_image)
+            }
+            true
         }
+
 
     }
 }
